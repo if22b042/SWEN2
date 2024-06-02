305ff3ac74706fdf123e62280080544a3fd96f12
if22b042 <148361921+if22b042@users.noreply.github.com>
2024-06-02 22:48:20 +0200

    Delete add_tourlog.bat

--- add_tourlog.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-curl -X POST "http://localhost:8080/tourlogs" -H "Content-Type: application/json" -d "{\"dateTime\": \"2024-05-13T10:00:00\", \"comment\": \"Great tour!\", \"difficulty\": \"Easy\", \"totalDistance\": 10, \"totalTime\": 120, \"rating\": 5, \"tourId\": 1}"
-pause
